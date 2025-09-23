00714dc87526ac66dd86816480bb88c37ce9d4f3
Nitin Vincent <nvincen2@crcfe02.crc.nd.edu>
2025-09-22 21:22:59 -0400

    Troubleshooting trim scripts

--- 01_QC/QC_Scripts/trim.sh
+++ 01_QC/QC_Scripts/trim.sh
@@ -8,18 +8,25 @@ ncores=$3
 
 mkdir -p ${OUTDIR}
 
-for f in "${SAMPDIR}"/*_R1_001.fastq.gz # for each sample
+for r1 in "${SAMPDIR}"/*_R1_001.fastq.gz # for each sample
 
 do
-    case "$f" in
+    echo "CURRENT INPUT FILE IS $f"
+    case "$r1" in
         *Undetermined*) continue ;;  # skip
     esac
-    echo "Processing $f"
-    n=${f%%_R1.fastq.gz} # strip part of file name
-    trimmomatic PE -threads ${ncores} ${n}_R1.fastq.gz  ${n}_R2.fastq.gz \
-    ${OUTDIR}/${n}_R1_trimmed.fastq.gz ${OUTDIR}/${n}_R1_unpaired.fastq.gz \
-    ${OUTDIR}/${n}_R2_trimmed.fastq.gz ${OUTDIR}/${n}_R2_unpaired.fastq.gz \
+    
+    f=${r1##*/}; base=${f%.fastq.gz}
+    echo "BASENAME IS $base"
+    
+    IFS=_ read -r -a p <<< "$base"; new="${p[0]}_${p[1]}_${p[2]}"
+    echo "Processing $new"
+    
+    n=${r1%%_R1_001.fastq.gz}
+    trimmomatic PE -threads ${ncores} ${n}_R1_001.fastq.gz  ${n}_R2_001.fastq.gz \
+    ${OUTDIR}/${new}_R1_trimmed.fastq.gz ${OUTDIR}/${new}_R1_unpaired.fastq.gz \
+    ${OUTDIR}/${new}_R2_trimmed.fastq.gz ${OUTDIR}/${new}_R2_unpaired.fastq.gz \
     ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
-    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
+    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 done
